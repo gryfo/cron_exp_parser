9b7c162c553d86584fb16b4e1b00821a25aab4c2
gryfo <gryfo.wpz@gmail.com>
2021-04-02 18:53:17 +0200

    Add function expanding asterix expression

--- cron_exp_parser.py
+++ cron_exp_parser.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+def expand_asterix(field_name):
+    if field_name == 'minute':
+        return [str(n) for n in range(0, 60)]
+    if field_name == 'hour':
+        return [str(n) for n in range(0, 24)]
+    if field_name == 'day of month':
+        return [str(n) for n in range(1, 32)]
+    if field_name == 'month':
+        return [str(n) for n in range(1, 13)]
+    if field_name == 'day of week':
+        return [str(n) for n in range(1, 8)]
+
+
 def field_parse(field_name, field_str):
     out_value = []
     if field_name == 'command':
@@ -15,28 +28,29 @@ def field_parse(field_name, field_str):
         a, b = field_str.split('-')
         out_value = [str(n) for n in range(int(a), int(b)+1)]
     elif field_str == '*':
-        out_value = f'every {field_name}'
+        out_value = ' '.join(expand_asterix(field_name))
     else:
         out_value = field_str
 
     return out_value
 
+
 def cron_exp_parse(exp):
     field_names = ['minute', 'hour', 'day of month', 'month', 'day of week', 'command']
     out_dict = {}
     parts = exp.split()
-
-
-    for p in parts:
-        fn = field_names[parts.index(p)]
-        v = field_parse(fn, p)
+    for p in range(len(parts)):
+        fn = field_names[p]
+        v = field_parse(fn, parts[p])
         out_dict[fn] = v
 
     return out_dict
+
 
 def print_table(d, w):
     for k, v in d.items():
         print(f"{k:<{w}} {' '.join(v) if isinstance(v, list) else v}")
 
+
 if __name__ == '__main__':
-    print_table(cron_exp_parse('*/15 0 1,15 * 1-5 /usr/bin/find'), 14)
+    print_table(cron_exp_parse('*/15 0 * * * /usr/bin/find'), 14)
